49c84d7950754b00c21cc499e451f4b404e763d8
Gregory <netsafe.g@gmail.com>
2020-12-11 14:12:21 +0500

    Add proxy middleware

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express')
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const app = express()
+const app = express();
 
 app.use(express.static('./dist/task-board-front'));
 
-app.get('/*', function(req, res) {
+app.all('/api/*', createProxyMiddleware({
+  target: process.env.API_URL,
+  changeOrigin: true,
+  pathRewrite: {'^/api': ''}
+}))
+
+app.get('/*', (_, res) => {
   res.sendFile('index.html', {root: 'dist/task-board-front'});
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200, () => { 
+  console.log(`Express server is listed on port ${process.env.PORT || 4200}`)
+});
